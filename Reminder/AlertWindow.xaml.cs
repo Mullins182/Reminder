36ec41c3c1e5a27ac2131cf62b95411214fcc45e
Mullins182 <Mullins182@t-online.de>
2024-12-07 10:46:41 +0100

    added new window for message showup, built grids for both windows, configured textboxes design and content, buttons and click event handler implemented.

---
+++ Reminder/AlertWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Reminder
+{
+    public partial class AlertWindow : Window
+    {
+        public AlertWindow()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            MessageBox.Text = MainWindow.notificationString;
+            MessageBox.IsReadOnly = true;
+            MessageBox.TextAlignment = TextAlignment.Center;
+            MessageBox.VerticalContentAlignment = VerticalAlignment.Center;
+            MessageBox.AcceptsReturn = true;
+            this.Topmost = true;
+
+        }
+
+        private void CloseMessage_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
