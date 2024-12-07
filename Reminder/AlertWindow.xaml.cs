666f040c99e23e3f9e06c4fe29b6b0133394e6ef
Mullins182 <Mullins182@t-online.de>
2024-12-08 00:23:52 +0100

    implemented and debugged doubleAnimation, integrated and configured event handler for textbox in MainWindow. Prg is functional !

--- Reminder/AlertWindow.xaml.cs
+++ Reminder/AlertWindow.xaml.cs
@@ -9,28 +9,53 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Windows.UI.Composition;
 
 namespace Reminder
 {
     public partial class AlertWindow : Window
     {
-        public AlertWindow()
+
+        private readonly DoubleAnimation TextboxAnimation = new();
+        private bool animationFinished = false;
+        private int animationTimerMsec = 3000;
+
+        public AlertWindow(string notification)
         {
             InitializeComponent();
-            Initialize();
+            Initialize(notification);
         }
 
-        private void Initialize()
+        private async void Initialize(string notification)
         {
-            MessageBox.Text = MainWindow.notificationString;
+
+            CloseMessage.Visibility = Visibility.Collapsed;
+
+            TextboxAnimation.Duration = TimeSpan.FromMilliseconds(animationTimerMsec);
+            TextboxAnimation.From = 0;
+            TextboxAnimation.To = 500;
+
+            MessageBox.Text = notification;
+            MessageBox.Width = 0;
             MessageBox.IsReadOnly = true;
             MessageBox.TextAlignment = TextAlignment.Center;
             MessageBox.VerticalContentAlignment = VerticalAlignment.Center;
             MessageBox.AcceptsReturn = true;
             this.Topmost = true;
 
+            await StartTextboxAnimation();
+
+            CloseMessage.Visibility = Visibility.Visible;
+        }
+
+        private async Task<bool> StartTextboxAnimation()
+        {
+            MessageBox.BeginAnimation(WidthProperty, TextboxAnimation);
+            await Task.Delay(animationTimerMsec);
+            return true;
         }
 
         private void CloseMessage_Click(object sender, RoutedEventArgs e)
--- Reminder/MainWindow.xaml.cs
+++ Reminder/MainWindow.xaml.cs
@@ -17,7 +17,8 @@ namespace Reminder
     {
 
         private readonly DispatcherTimer Timer = new();
-        public static string notificationString = "";
+        private static readonly string stdReminderText = "Enter Your Notification Message !";
+
 
         public MainWindow()
         {
@@ -27,23 +28,61 @@ namespace Reminder
 
         public void Initialize()
         {
+            this.GotFocus += MainWindow_GotFocus;
+            ReminderText.MouseLeave += ReminderText_MouseLeave;
+            ReminderText.MouseEnter += ReminderText_MouseEnter;
+            ReminderText.TextChanged += ReminderText_TextChanged;
+
             Btn_StartReminder.Content = "Start\nTimer";
 
+            ReminderText.CaretBrush = new SolidColorBrush(Colors.Transparent);
+            ReminderText.IsReadOnly = true;
+            ReminderText.Text = stdReminderText;
             ReminderText.TextAlignment = TextAlignment.Center;
             ReminderText.VerticalContentAlignment = VerticalAlignment.Center;
             ReminderText.AcceptsReturn = true;
+            ReminderText.Focus();
 
-            Timer.Interval = TimeSpan.FromSeconds(10);
+            Timer.Interval = TimeSpan.FromSeconds(4);
             Timer.Tick += Notification_Tick;
+        }
+
+        private void MainWindow_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ReminderText.Focus();
+        }
+
+        private void ReminderText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void ReminderText_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ReminderText.IsReadOnly = false;
+            ReminderText.CaretBrush = new SolidColorBrush(Colors.Gold);
+            ReminderText.CaretIndex = ReminderText.Text.Length;
+            ReminderText.Text = ReminderText.Text == stdReminderText ? "" : ReminderText.Text;
+        }
+        private void ReminderText_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ReminderText.Text = ReminderText.Text == "" ? stdReminderText : ReminderText.Text;
+            ReminderText.CaretBrush = new SolidColorBrush(Colors.Transparent);
+            ReminderText.IsReadOnly = true;
         }
 
         private void Notification_Tick(object? sender, EventArgs e)
         {
-            AlertWindow Notification = new();
+            AlertWindow Notification = new(ReminderText.Text);
 
             Notification.Show();
 
             Timer.Stop();
+        }
+
+        private bool CheckReminderText()
+        {
+            return ReminderText.Text == stdReminderText ? false : true;
         }
 
         // Button Click-Eventhandler
@@ -54,8 +93,10 @@ namespace Reminder
 
         private void Btn_StartReminder_Click(object sender, RoutedEventArgs e)
         {
-            Timer.Start();
-            notificationString = ReminderText.Text;
+            if (CheckReminderText())
+            {
+                Timer.Start();
+            }
         }
     }
 }
